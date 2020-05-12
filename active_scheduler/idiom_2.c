975b9d398a42e13e9468d0accee66a7b856cddd8
cottonlist <35421808+cottonlist@users.noreply.github.com>
2020-05-12 19:27:23 +0900

    Add files via upload

--- active_scheduler/idiom_2.c
+++ active_scheduler/idiom_2.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#include <assert.h>
 #include <unistd.h>
+#include <string.h>
 
-int a = 0;
+typedef struct
+{	
+	int score;
+}student;
 
-void* func(void* x) {
-	a = 1;
-	assert(a!=2);
+void *
+func1 (student *std)
+{
+	if (std)
+	{
+		printf("%d", std->score);
+	}
+	return NULL;
+}
+
+void *
+func2 (student *std)
+{
+	std = NULL;
 	return NULL;
 }
 
 int main() {
-	pthread_t t;
-	pthread_create(&t, NULL, func, NULL);
-	
-	a = 2;
+	student taro = {80};
+	student *student1 = &taro;
 
-	pthread_join(t, NULL);
+	pthread_t t1;
+	pthread_t t2;
+
+	pthread_create(&t1, NULL, func1, student1);
+	pthread_create(&t2, NULL, func2, student1);
+	
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	exit(0);
 }
