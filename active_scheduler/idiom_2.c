ef970eb47562c51fb4f74f2cbe5085a90a1492b4
cottonlist <35421808+cottonlist@users.noreply.github.com>
2020-05-12 20:48:53 +0900

    Add files via upload

--- active_scheduler/idiom_2.c
+++ active_scheduler/idiom_2.c
@@ -2,42 +2,138 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <string.h>
 
 typedef struct
 {	
 	int score;
 }student;
 
-void *
-func1 (student *std)
+typedef struct {
+	pthread_mutex_t lock;
+	pthread_cond_t cond;
+	int isSignaled;
+} trigger;
+
+trigger *
+trigger_init()
 {
-	if (std)
+	trigger *t = (trigger *) malloc(sizeof(trigger));
+
+	pthread_mutex_init(&(t->lock), NULL);
+	pthread_cond_init(&(t->cond), NULL);
+	t->isSignaled = 0;
+	return t;
+}
+
+void
+trigger_destroy(trigger *t)
+{
+	pthread_mutex_destroy(&(t->lock));
+	pthread_cond_destroy(&(t->cond));
+	free(t);
+}
+
+void
+trigger_signal(trigger *t)
+{
+	pthread_mutex_lock(&(t->lock));
+	pthread_cond_signal(&(t->cond));
+	t->isSignaled = 1;
+	pthread_mutex_unlock(&(t->lock));
+}
+
+void
+trigger_wait(trigger *t)
+{
+	pthread_mutex_lock(&(t->lock));
+	while (!t->isSignaled){
+		pthread_cond_wait(&(t->cond), &(t->lock));
+	}
+	t->isSignaled = 0;
+	pthread_mutex_unlock(&(t->lock));
+}
+
+trigger *trigger1 = NULL;
+trigger *trigger2 = NULL;
+
+void
+begin (int index)
+{
+	fprintf(stderr, "begin(%d)\n", index);
+	if (index == 1001) {
+		// do nothing
+	} else if (index == 2001){
+		trigger_wait(trigger2);
+	} else if (index == 1002){
+		trigger_wait(trigger1);
+	}
+}
+
+void
+end (int index)
+{
+	fprintf(stderr, "end(%d)\n", index);
+	if (index == 1001)
 	{
-		printf("%d", std->score);
+		trigger_signal(trigger2);
+	} else if (index == 2001)
+	{
+		trigger_signal(trigger1);
+	}
+}
+
+void
+crash()
+{
+	fprintf(stderr, "crash()\n");
+}
+
+void *
+func1 (void *std)
+{
+	begin(1001);
+	if (((student *)std)->score >= 60)
+	{
+		end(1001);
+		begin(1002);
+		if (((student *)std)->score < 60)
+		{
+			crash();
+		}
+		end(1002);
 	}
 	return NULL;
 }
 
 void *
-func2 (student *std)
+func2 (void *std)
 {
-	std = NULL;
+	begin(2001);
+	((student *)std)->score = 30;
+	//std = NULL;
+	end(2001);
 	return NULL;
 }
 
 int main() {
 	student taro = {80};
+
 	student *student1 = &taro;
+
+	trigger1 = trigger_init();
+	trigger2 = trigger_init();
 
 	pthread_t t1;
 	pthread_t t2;
 
-	pthread_create(&t1, NULL, func1, student1);
-	pthread_create(&t2, NULL, func2, student1);
+	pthread_create(&t1, NULL, func1, (void*)student1);
+	pthread_create(&t2, NULL, func2, (void*)student1);
 	
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+
+	trigger_destroy(trigger1);
+	trigger_destroy(trigger2);
 
 	exit(0);
 }
