6e6028aff7a7f0fe3d96c30b4747b2fcf12cd2bc
cottonlist <35421808+cottonlist@users.noreply.github.com>
2020-05-14 13:26:00 +0200

    Add files via upload

--- active_scheduler/idiom_5.c
+++ active_scheduler/idiom_5.c
@@ -1,34 +1,100 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+
+#include "trigger.h"
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
-void* foo(void* args){
+trigger *trigger1 = NULL;
+trigger *trigger2 = NULL;
+
+void
+begin(int index)
+{
+	fprintf(stderr, "begin(%d)\n", index);
+	if (index == 2001)
+	{
+		trigger_wait(trigger2);
+	} else if (index == 1002)
+	{
+		trigger_wait(trigger1);
+	}
+}
+
+void
+end(int index)
+{
+	fprintf(stderr, "end(%d)\n", index);
+	if (index == 1001)
+	{
+		trigger_signal(trigger2);
+	} else if (index == 2001)
+	{
+		trigger_signal(trigger1);
+	}
+}
+
+void *
+func1(void *args)
+{
+	begin(1001);
 	pthread_mutex_lock(&mutex1);
+	end(1001);
+	begin(1002);
 	pthread_mutex_lock(&mutex2);
-	printf("foo\n");
-	pthread_mutex_lock(&mutex2);
-	pthread_mutex_lock(&mutex1);
+	end(1002);
+	begin(1003);
+	printf("failed\n");
+	end(1003);
+	begin(1004);
+	pthread_mutex_unlock(&mutex2);
+	end(1004);
+	begin(1005);
+	pthread_mutex_unlock(&mutex1);
+	end(1005);
+
 	return NULL;
 }
 
-void* bar(void* args){	
+void *
+func2(void *args)
+{	
+	begin(2001);
 	pthread_mutex_lock(&mutex2);
+	end(2001);
+	begin(2002);
 	pthread_mutex_lock(&mutex1);
-	printf("bar\n");
-	pthread_mutex_lock(&mutex1);
-	pthread_mutex_lock(&mutex2);
+	end(2002);
+	begin(2003);
+	printf("failed\n");
+	end(2003);
+	begin(2004);
+	pthread_mutex_unlock(&mutex1);
+	end(2004);
+	begin(2005);
+	pthread_mutex_unlock(&mutex2);
+	end(2005);
+
 	return NULL;
 }
 
 int main() {
-	pthread_t t1, t2;
-	pthread_create(&t1, NULL, foo, NULL);
-	pthread_create(&t2, NULL, bar, NULL);
+	trigger1 = trigger_init();
+	trigger2 = trigger_init();
+
+	pthread_t t1;
+	pthread_t t2;
+
+	pthread_create(&t1, NULL, func1, NULL);
+	pthread_create(&t2, NULL, func2, NULL);
 
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 	
-	return 0;
+	trigger_destroy(trigger1);
+	trigger_destroy(trigger2);
+
+	exit(0);
 }
