382493940b38354c6743c438c40893dbaa2cd08e
cottonlist <35421808+cottonlist@users.noreply.github.com>
2020-05-19 05:08:10 +0900

    Generalization

--- active_scheduler/idiom_1.c
+++ active_scheduler/idiom_1.c
@@ -5,9 +5,15 @@
 
 #include "trigger.h"
 
-#define EXEC_LENGTH 3
+// int exec_order[] = {1001, 2001, 1002};
+// int exec_order[] = {2001, 1002, 2003, 1004};
+// int exec_order[] = {1002, 2003, 1003, 2004};
+// int exec_order[] = {2002, 1002, 2003, 1003, 2004, 1004};
+int exec_order[] = {1001, 2003, 1004, 2004};
 
-int exec_order[EXEC_LENGTH] = {1001, 2001, 1002};
+int exec_length = sizeof(exec_order)/sizeof(int);
+
+// int exec_length = sizeof(exec_order);
 
 int a = 0;
 
@@ -17,50 +23,50 @@ trigger *trigger2 = NULL;
 void
 begin(int index)
 {
+	int isContained = 0;
+	for (int i = 0; i < exec_length; ++i)
+	{
+		if (index == exec_order[i])
+		{
+			isContained = 1;
+		}
+	}
 	fprintf(stderr, "begin(%d)\n", index);
-	if ((index != exec_order[0]) && (index/1000 == 2))
+	if (index != exec_order[0] && isContained == 1)
 	{
-		trigger_wait(trigger2);
-	} else if ((index != exec_order[0]) && (index/1000 == 1))
-	{
-		trigger_wait(trigger1);
+		if (index/1000 == 2)
+		{
+			trigger_wait(trigger2);
+		} else if (index/1000 == 1)
+		{
+			trigger_wait(trigger1);
+		}
 	}
 }
 
 void
 end(int index)
 {
+	int isContained = 0;
+	for (int i = 0; i < exec_length; ++i)
+	{
+		if (index == exec_order[i])
+		{
+			isContained = 1;
+		}
+	}
 	fprintf(stderr, "end(%d)\n", index);
-	if ((index != exec_order[EXEC_LENGTH-1]) && (index/1000 == 1))
+	if (index != exec_order[exec_length-1] && isContained == 1)
 	{
-		trigger_signal(trigger2);
-	} else if ((index != exec_order[EXEC_LENGTH-1]) && (index/1000 == 2))
-	{
-		trigger_signal(trigger1);
-	} 
+		if (index/1000 == 1)
+		{
+			trigger_signal(trigger2);
+		} else if (index/1000 == 2)
+		{
+			trigger_signal(trigger1);
+		}
+	}
 }
-
-// void
-// begin(int index)
-// {
-// 	fprintf(stderr, "begin(%d)\n", index);
-// 	if (index == exe_order[1]) {
-// 		trigger_wait(trigger2);
-// 	} else if (index == exe_order[2]) {
-// 		trigger_wait(trigger1);
-// 	}
-// }
-
-// void
-// end(int index)
-// {
-// 	fprintf(stderr, "end(%d)\n", index);
-// 	if (index == exe_order[0]) {
-// 		trigger_signal(trigger2);
-// 	} else if (index == exe_order[1]) {
-// 		trigger_signal(trigger1);
-// 	} 
-// }
 
 void
 crash()
@@ -74,9 +80,29 @@ func1(void *arg)
 	begin(1001);
 	a = 1;
 	end(1001);
+
 	begin(1002);
-	a = 2;
+	a = 1;
 	end(1002);
+
+	begin(1003);
+	a = 1;
+	end(1003);
+
+	begin(1004);
+	a = 1;
+	end(1004);
+
+	begin(1005);
+	a = 1;
+	end(1005);
+
+	// begin(1001);
+	// a = 1;
+	// end(1001);
+	// begin(1002);
+	// a = 2;
+	// end(1002);
 	return NULL;
 }
 
@@ -84,10 +110,29 @@ void *
 func2 (void *arg)
 {
 	begin(2001);
-	if (a==1) {
-		crash();
-	}
+	a = 1;
 	end(2001);
+
+	begin(2002);
+	a = 1;
+	end(2002);
+
+	begin(2003);
+	a = 1;
+	end(2003);
+
+	begin(2004);
+	a = 1;
+	end(2004);
+
+	begin(2005);
+	a = 1;
+	end(2005);
+	// begin(2001);
+	// if (a==1) {
+	// 	crash();
+	// }
+	// end(2001);
 	return NULL;
 }
 
